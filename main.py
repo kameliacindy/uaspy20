7634da911d7fa9896328af7f47e404c311e1e753
kameliacindy <kamelcindy60@gmail.com>
2020-01-13 11:30:35 +0700

    Upload

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-class MainApp():
+from core.baseapp import BaseApp
+class MainApp(BaseApp):
     def __init__(self):
         self.books = []
 
+
+    def list_book(self):
+        
 
 if __name__ == "__main__":
     app = MainApp()
